30e1992efefcdc68edd897c1cc15501aff0f8e2a
fengjutian <2640111081@qq.com>
2019-11-06 17:39:01 +0800

    save

---
+++ src/component/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+// import './Home.scss';
+
+class Home extends Component {
+  render() {
+    return (
+      <div >
+        kkkkkk
+      </div>
+    )
+  }
+}
+
+
+export default Home;
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './component/app/App.js';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, HashRouter, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './component/App.js';
+import Home from './component/Home.js';
+import Home1 from './component/Home1.js';
+
+ReactDOM.render((
+  <Router>
+      <Route path='/' component={Home}></Route>
+      <Route path='/app' component={App}></Route>
+      <Route path='/home' component={Home1}></Route>
+  </Router>
+  // <HashRouter>
+  //       <Switch>
+  //         <Route path='/' component={Home}></Route>
+  //         <Route path='/app' component={App}></Route>
+  //         <Route path='/home' component={Home1}></Route>
+  //       </Switch>
+  //   </HashRouter>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
